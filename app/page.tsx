5c4fccf2a6d03d2a2d89f2006abc6d5514ec3ffc
nja93 <njanjal@gmail.com>
2024-02-26 17:00:54 +0300

    changed bg mage, adjusted text style

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,14 @@ export default function Home() {
               Trained and Certified Geologists
             </span>
           </h1>
-            <p className="text-yellow-50  hidden md:block">
-            Welcome to GeoK, your premier destination for connecting with qualified Kenyan Geologists. Our platform brings together a diverse community of geology experts, ready to assist you in navigating the complexities of the Earth's terrain. Whether you're exploring for resources, studying geological phenomena, or seeking professional guidance, our network offers the expertise you need. Join us in unlocking the secrets of the Earth's past, present, and future with our dedicated team of skilled geologists
+            <p className="text-black hidden md:block">
+           <strong> Welcome to GeoK, your premier destination for connecting with qualified Kenyan Geologists. Our platform brings together a diverse community of geology experts, ready to assist you in navigating the complexities of the Earth's terrain. Whether you're exploring for resources, researching geological phenomena, or seeking professional guidance, our network offers the expertise you need. Join us in unlocking the secrets of the Earth's past, present, and future with our dedicated team of skilled geologists
+           </strong>
             </p>
             <div className="flex-col md:flex-row hidden md:flex gap-5">
               <Link href="/my-skills" 
               className="rounded-[20px] group relative bg-neutral-900  opacity-40  hover:bg-blue-300 px-5 py-3 text-lg text-white max-w-[200px]">
-                Learn more
+                Learn moren
               </Link>
               <Link href="/my-projects"
               className="rounded-[20px] group relative  border bg-yellow-900 hover:bg-blue-300 px-5 py-3 text-lg text-white max-w-[200px]">
