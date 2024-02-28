10d102595ff12b0e6bcf623cf50b3761f90c8c59
nja93 <njanjalchege@gmail.com>
2024-02-28 03:44:49 +0300

    implemented css classes to search results

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,11 @@
 import db from '@/db'
+import { Fragment } from "react";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+
 
 const Search = async ({
   params,
@@ -35,34 +42,112 @@ const Search = async ({
       }
     }
   })
+  const specializations = data.reduce((acc: any, curr: any) => {
+    const specialization = acc.find((s) => s.specialization! === curr.title);
+    const person = {
+      membershipNo: curr.membershipNo,
+      name: `${curr.profile?.firstName} ${curr.profile?.lastName}`,
+      specialization: curr.title,
+      email: curr.profile?.email ?? 'n/a',
+      phone: curr.profile?.phone1 ?? 'n/a',
+    };
+    if (specialization) {
+      specialization.people.push(person);
+    } else {
 
+      acc.push({ name: curr.title, people: [person] });
+    }
+    return acc;
+  }, []);
 
   return (
-    <div>
-      <div>Search</div>
-      <div>
-        <table>
-          <thead>
-            <th>Membership No.</th>
-            <th>Name</th>
-            <th>Specialization</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-          </thead>
-          <tbody>
-            {data.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.membershipNo}</td>
-                  <td>{`${item.profile?.firstName} ${item.profile?.lastName}`}</td>
-                  <td>{item.title}</td>
-                  <td>{item.profile?.phone1}</td>
-                  <td><a href={`mailto:${item.profile?.email}`}>{item.profile?.email}</a></td>
-                </tr>)
-            })}
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">
+            Users
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all the users in your account including their name, title,
+            email and role.
+          </p>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full">
+              <thead className="bg-white">
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                  >
+                    Membership No
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Name
+                  </th>
 
-          </tbody>
-        </table>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Phone
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Email
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {specializations.map((specialization) => (
+                  <Fragment key={specialization.name}>
+                    <tr className="border-t border-gray-200">
+                      <th
+                        colSpan={5}
+                        scope="colgroup"
+                        className="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                      >
+                        {specialization.name}
+                      </th>
+                    </tr>
+                    {specialization.people.map((person, personIdx) => (
+                      <tr
+                        key={person.membershipNo}
+                        className={classNames(
+                          personIdx === 0
+                            ? "border-gray-300"
+                            : "border-gray-200",
+                          "border-t"
+                        )}
+                      >
+                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                          {person.membershipNo}
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          {person.name}
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          {person.phone}
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <a href={`mailto:${person.email}`}>{person.email}</a>
+                        </td>
+                      </tr>
+                    ))}
+                  </Fragment>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   )
